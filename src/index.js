7af53f4afcfb1612d78ecdcb26882c49212415bf
Nicolai Gorden <nicolaigorden@gmail.com>
2022-02-01 17:26:10 -0600

    commit tree 4d560502af095e61f211df078a6352764fab71d1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    let dogUl = document.getElementById("dog-breeds");
+    
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json())
+    .then(handleImageAppending)
+
+    makeFetch()
+    .then(response => {
+        let arrOfDogNames = Object.keys(response.message);
+        arrOfDogNames.forEach(breed => {
+            dogUl.innerHTML += `<li data-info="breed">${breed}</li>`
+        });
+    });
+
+    dogUl.addEventListener("click", function(event){
+        if (event.target.dataset.info === "breed"){
+            event.target.style.color = "blue"
+        }
+    });
+
+    let breedDropdown = document.getElementById("breed-dropdown")
+    breedDropdown.addEventListener('change', (event) => {
+        makeFetch()
+        .then(response => {
+            let arrOfDogNames = Object.keys(response.message);
+            let filteredArray = arrOfDogNames.filter(breed=> {
+                return breed.startsWith(event.target.value)
+            })
+            removeChildNodes(dogUl)
+            filteredArray.forEach(breed => {
+                dogUl.innerHTML += `<li data-info="breed">${breed}</li>`
+            });
+        });
+    });
+    //DOMcontentloaded
+});
+
+let elements = document.getElementById("li")
+
+
+function handleImageAppending(jsonObject){
+    let dogImageContainer = document.getElementById("dog-image-container");
+    let arrOfDogsURLs = jsonObject.message
+    arrOfDogsURLs.forEach(url => dogImageContainer.innerHTML += makeImageTagString(url))
+}
+
+
+function makeImageTagString(url){
+    return `<img src="${url}"/>`
+}
+
+function makeFetch() {
+    return fetch("https://dog.ceo/api/breeds/list/all")
+    .then(response => response.json())
+}
+
+function removeChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
+}
